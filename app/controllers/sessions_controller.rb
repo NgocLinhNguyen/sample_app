c9b189169510732119fd0e802ea5196b0fff3f31
NgocLinhNguyen <ngoclinhnguyen.hust@gmail.com>
2016-03-09 08:55:36 +0700

    Exercise chap 8

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,12 +2,12 @@ class SessionsController < ApplicationController
   def new
   end
   def create
-  	user = User.find_by(email: params[:session][:email].downcase)
-    if user && user.authenticate(params[:session][:password])
-      log_in user
-      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
+  	@user = User.find_by(email: params[:session][:email].downcase)
+    if @user && @user.authenticate(params[:session][:password])
+      log_in @user
+      params[:session][:remember_me] == '1' ? remember(@user) : forget(@user)
 
-      redirect_to user
+      redirect_to @user
 
       # Log the user in and redirect to the user's show page.
     else
